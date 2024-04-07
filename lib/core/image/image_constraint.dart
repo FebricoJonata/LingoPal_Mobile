dcf1d16a2da8269e1ad6f7f2f0092263def76921
Fabian <fabian.djaja@gmail.com>
2024-04-07 19:30:58 +0700

    Landing Pages

--- lib/core/image/image_constraint.dart
+++ lib/core/image/image_constraint.dart
@@ -1,9 +1,15 @@
 class AssetConstraints {
   static const baseImages = 'assets/images';
-  static const baseBgImages = 'assets/images/background_asset';
+  static const baseBgImages = '$baseImages/background_asset';
+  static const baseLandingImages = "$baseImages/carousel_assets";
 
   //background
   static const bgIntroTop = "$baseBgImages/bg_intro_top.png";
   static const bgIntroBot = "$baseBgImages/bg_intro_bot.png";
   static const bgAppLogo = "$baseBgImages/bg_app_logo.png";
+
+  //landing
+  static const page1 = "$baseLandingImages/page1.png";
+  static const page2 = "$baseLandingImages/page2.png";
+  static const page3 = "$baseLandingImages/page3.png";
 }
---
+++ lib/presentation/pages/landing_page/controller/page_view_controller.dart
@@ -0,0 +1,43 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:lingo_pal_mobile/presentation/pages/landing_page/widgets/page_view/page_1.dart';
+import 'package:lingo_pal_mobile/presentation/pages/landing_page/widgets/page_view/page_2.dart';
+import 'package:lingo_pal_mobile/presentation/pages/landing_page/widgets/page_view/page_3.dart';
+
+class PageViewController extends GetxController {
+  late PageController pageController;
+  final List<Widget> pages = [const Page1(), const Page2(), const Page3()];
+  RxInt currentPageIndex = 0.obs;
+  late Timer _timer;
+
+  @override
+  void onInit() {
+    super.onInit();
+    pageController = PageController(initialPage: 0);
+    // _startAutoSlide();
+  }
+
+  @override
+  void onClose() {
+    _timer.cancel();
+    pageController.dispose();
+    super.onClose();
+  }
+
+  // void _startAutoSlide() {
+  //   _timer = Timer.periodic(const Duration(seconds: 5), (timer) {
+  //     if (currentPageIndex.value < pages.length - 1) {
+  //       currentPageIndex.value++;
+  //     } else {
+  //       currentPageIndex.value = 0;
+  //     }
+  //     pageController.animateToPage(
+  //       currentPageIndex.value,
+  //       duration: const Duration(milliseconds: 500),
+  //       curve: Curves.easeInOut,
+  //     );
+  //   });
+  // }
+}
