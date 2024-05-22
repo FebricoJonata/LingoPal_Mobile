4a6dad574dae4d61afca56823fa2ee7aacd3c784
ClaudyaSalim <claudya.salim@gmail.com>
2024-05-22 16:17:09 +0700

    change search bar

--- lib/presentation/view/dictionary_page/widgets/search_bar_dictonary.dart
+++ lib/presentation/view/dictionary_page/widgets/search_bar_dictonary.dart
@@ -1,31 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:lingo_pal_mobile/core/color/color_constraint.dart';
+import 'package:lingo_pal_mobile/presentation/view/components/primary_btn_reusable.dart';
 
 class SearchBarDictonary extends StatelessWidget {
-  const SearchBarDictonary({super.key});
+  SearchBarDictonary({super.key});
+
+  TextEditingController searchController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 10),
-      decoration: BoxDecoration(
-        color: MyColors.white,
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: const Row(
-        children: [
-          Padding(padding: EdgeInsets.all(8.0), child: Icon(Icons.search)),
-          Expanded(
-            child: TextField(
-              decoration: InputDecoration(
-                hintText: 'Search for words...',
-                border: InputBorder.none,
-                contentPadding: EdgeInsets.symmetric(vertical: 12),
-              ),
-            ),
-          ),
-        ],
-      ),
+
+    return SearchBar(
+      controller: searchController,
+      hintText: "Search for words...",
+      leading: Icon(Icons.search),
+      elevation: MaterialStatePropertyAll(0),
+      padding: MaterialStatePropertyAll(const EdgeInsets.symmetric(horizontal: 10)),
+      surfaceTintColor: MaterialStatePropertyAll(MyColors.white),
+      shape: MaterialStatePropertyAll(RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0))),
+      onSubmitted: (value) {
+        print(value);
+      },
     );
+
+    // return Container(
+    //   padding: const EdgeInsets.symmetric(horizontal: 10),
+    //   decoration: BoxDecoration(
+    //     color: MyColors.white,
+    //     borderRadius: BorderRadius.circular(10),
+    //   ),
+    //   child: Row(
+    //     children: [
+    //       Padding(padding: EdgeInsets.all(8.0), child: Icon(Icons.search)),
+    //       Expanded(
+    //         child: TextField(
+    //           decoration: InputDecoration(
+    //             hintText: 'Search for words...',
+    //             border: InputBorder.none,
+    //             contentPadding: EdgeInsets.symmetric(vertical: 12),
+    //           ),
+    //           controller: searchController,
+    //         ),
+    //       ),
+    //       Padding(padding: EdgeInsets.all(8.0), child: Icon(Icons.close)),
+    //       PrimaryBtn(btnText: "Search", width: 150.w, height: 80.h)
+    //     ],
+    //   ),
+    // );
   }
 }
