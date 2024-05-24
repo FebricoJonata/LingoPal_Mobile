73b4b09fd4ef486493a21fd894228919f9684baf
ClaudyaSalim <claudya.salim@gmail.com>
2024-05-24 15:49:52 +0700

    word card

---
+++ lib/presentation/view/dictionary_page/widgets/word_card.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class WordCard extends StatefulWidget {
+  const WordCard({super.key});
+
+  @override
+  State<WordCard> createState() => _WordCardState();
+}
+
+class _WordCardState extends State<WordCard> {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Container(),
+    );
+  }
+}
