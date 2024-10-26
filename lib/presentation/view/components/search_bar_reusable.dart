8be0c958ebbaa8fc2a35e09d3d4dce33b699ead9
ClaudyaSalim <claudya.salim@gmail.com>
2024-10-26 16:26:28 +0700

    modif UI searchbar

--- lib/presentation/view/components/search_bar_reusable.dart
+++ lib/presentation/view/components/search_bar_reusable.dart
@@ -2,8 +2,10 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
 import 'package:lingo_pal_mobile/core/color/color_constraint.dart';
 import 'package:lingo_pal_mobile/presentation/view/components/primary_btn_reusable.dart';
+import 'package:lingo_pal_mobile/presentation/view/material_page/material_page.dart';
 
 class ReuseSearchBar extends StatefulWidget {
   ReuseSearchBar({super.key, required this.setSearchMethod, required this.searchWord});
@@ -21,18 +23,19 @@ class _ReuseSearchBarState extends State<ReuseSearchBar> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 10),
+      padding: const EdgeInsets.fromLTRB(8, 0, 0, 0),
       decoration: BoxDecoration(
         color: MyColors.white,
         borderRadius: BorderRadius.circular(10),
       ),
+      height: 150.h,
       child: Row(
         children: [
           // Padding(padding: EdgeInsets.all(8.0), child: Icon(Icons.search)),
           Expanded(
             child: SearchBar(
               controller: searchController..text = widget.searchWord,
-              hintText: "Search ...",
+              hintText: "Type keywords ...",
               leading: Icon(Icons.search),
               elevation: WidgetStatePropertyAll(0),
               padding: WidgetStatePropertyAll(const EdgeInsets.symmetric(horizontal: 8)),
@@ -57,7 +60,7 @@ class _ReuseSearchBarState extends State<ReuseSearchBar> {
           PrimaryBtn(
             btnText: "Search",
             width: 180.w,
-            height: 150.h,
+            height: context.height,
             onClick: () {
 
               print(searchController.text);
